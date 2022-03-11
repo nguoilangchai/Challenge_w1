1b30db4f6887c1e082a366afd648b5b311ead6c9
nguoilangchai <hoxuantrieu20022000@gmail.com>
2022-03-11 21:47:57 +0700

    firtly update

---
+++ app/src/main/java/com/example/bai1/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.bai1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+//        setContentView(R.layout.activity_on_boarding_on)
+//        setContentView(R.layout.activity_sign_up)
+//        setContentView(R.layout.activity_login)
+//        setContentView(R.layout.verification_code)
+    }
+}
